b709b63a286f9fa8951736e8bb0a072db52f3950
des1re26 <des1re26@mail.ru>
2026-05-28 21:05:05 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
+// Copyright 2021 NNTU-CS
 #include <iostream>
+#include <string>
+
 #include "bst.h"
 
-// Объявления функций из alg.cpp
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
 
